85eeab67184d1e89e271fd10515764c1f1df2930
Glen Jolley <novaxeros@live.co.uk>
2020-05-26 16:50:24 +0100

    Finally managed to get WPF to update quickly: don't use binding, and use a DispatchTimer to update code behind and redraw UniformGrid.

--- Game Of Life (WPF)/Views/MainWindow.xaml.cs
+++ Game Of Life (WPF)/Views/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Game_Of_Life__WPF_
 {
@@ -24,32 +25,102 @@ namespace Game_Of_Life__WPF_
     {
 
         private Universe uni;
+        private DispatcherTimer dTimer = new DispatcherTimer();
 
         public MainWindow()
         {
-            
-            int row = 25;
-            int col = 50;
+
+            InitializeComponent();
+            dTimer.Tick += new EventHandler(dTimer_Tick);
+            dTimer.Interval = new TimeSpan(0, 0, 0, 0, 200);
+
+            int row = 50;
+            int col = 100;
             
             uni = new Universe(row, col, 50);
 
             this.DataContext = uni;
 
-            InitializeComponent();
+            LifeGrid.Rows = row;
+            LifeGrid.Columns = col;
+
+            initGrid();
+            drawGrid();
+
+        }
+
+        public void initGrid()
+        {
+
+            for (int i = 0; i < (uni.Length * uni.Width); i++)
+            {
+
+                Rectangle r = new Rectangle() { Fill = Brushes.White };
+
+                LifeGrid.Children.Add(r);
+
+            }
+
+        }
+
+        private void dTimer_Tick(object sender, EventArgs e)
+        {
+            Step();
+        }
+
+        public void drawGrid()
+        {
+
+            for (int i = 0; i < (uni.Length * uni.Width); i++)
+            {
+
+                this.Dispatcher.Invoke(() =>
+                {
+
+                    Rectangle r = (Rectangle)LifeGrid.Children[i];
+
+                    if (r.Fill != uni.Life[i].Brush)
+                    {
+                        r.Fill = (Brush)uni.Life[i].Brush;
+                    }
+
+                });
+
+            }
+
+        }
+
+        private void Step()
+        {
+            uni.CalculateGeneration();
+            drawGrid();           
+        }
+
+        public void ToggleTimer()
+        {
+
+            if (dTimer.IsEnabled)
+            {
+                dTimer.Stop();
+            }
+            else
+            {
+                dTimer.Start();
+            }
 
         }
 
         public void DoGenerations(int n)
         {
             for (int i = 0; i < n; i++)
-            {
+            {                
                 uni.CalculateGeneration();
             }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            uni.ToggleTimer();  
+           ToggleTimer();
         }
     }
 }
